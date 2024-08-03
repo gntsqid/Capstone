aff3b362061564c26b61dd1d8e5c85f0b228387e
gntsqid <132708602+gntsqid@users.noreply.github.com>
2024-08-03 11:15:55 -0700

    Create transmitter.py

---
+++ LoRa/transmitter.py
@@ -0,0 +1,108 @@
+# THIS IS MY OVERWRITE OF THE ORIGINAL
+# VERSION CONTROL IS USED
+# THIS CODE IS DESIGNED FOR KITSUNE
+
+# Import Python System Libraries
+import time
+# Import Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# Import the SSD1306 module.
+import adafruit_ssd1306
+# Import RFM9x
+import adafruit_rfm9x
+
+import socket
+
+# Button A
+btnA = DigitalInOut(board.D5)
+btnA.direction = Direction.INPUT
+btnA.pull = Pull.UP
+
+# Button B
+btnB = DigitalInOut(board.D6)
+btnB.direction = Direction.INPUT
+btnB.pull = Pull.UP
+
+# Button C
+btnC = DigitalInOut(board.D12)
+btnC.direction = Direction.INPUT
+btnC.pull = Pull.UP
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# 128x32 OLED Display
+reset_pin = DigitalInOut(board.D4)
+display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+# Clear the display.
+display.fill(0)
+display.show()
+width = display.width
+height = display.height
+
+# Configure LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x.tx_power = 23
+prev_packet = None
+
+# Grabbing Hostname
+hostname = socket.gethostname()
+hostname_str = f"{hostname}\r\n"
+
+# GPS data file path
+gps_data_file = '/home/ronin/Documents/GPS/gps_data.txt'
+
+while True:
+    packet = None
+    # draw a box to clear the image
+    display.fill(0)
+    display.text('RasPi LoRa', 35, 0, 1)
+
+    # check for packet rx
+    packet = rfm9x.receive()
+    if packet is None:
+        display.show()
+        display.text('- Waiting for PKT -', 15, 20, 1)
+    else:
+        # Display the packet text and rssi
+        display.fill(0)
+        prev_packet = packet
+        packet_text = str(prev_packet, "utf-8")
+        display.text('RX: ', 0, 0, 1)
+        display.text(packet_text, 25, 0, 1)
+        time.sleep(1)
+
+    if not btnA.value:
+        # Send Button A
+        display.fill(0)
+        button_a_data = bytes(hostname_str, "utf-8")
+        rfm9x.send(button_a_data)
+        display.text('Sent Button A!', 25, 15, 1)
+    elif not btnB.value:
+        # Send GPS data with Button B
+        try:
+            with open(gps_data_file, 'r') as file:
+                gps_data = file.read().strip()
+                if gps_data:
+                    gps_data_bytes = bytes(gps_data + "\r\n", "utf-8")
+                    rfm9x.send(gps_data_bytes)
+                    display.fill(0)
+                    display.text('Sent GPS Data!', 25, 15, 1)
+                    display.show()
+        except FileNotFoundError:
+            display.fill(0)
+            display.text('GPS Data Not Found', 15, 15, 1)
+    elif not btnC.value:
+        # Send Button C
+        display.fill(0)
+        button_c_data = bytes("Button C!\r\n", "utf-8")
+        rfm9x.send(button_c_data)
+        display.text('Sent Button C!', 25, 15, 1)
+
+    display.show()
+    time.sleep(0.1)
